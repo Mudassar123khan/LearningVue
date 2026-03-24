fa20ad3e874c26f3345b0651c7065e9e496ef858
Mudassir Khan <12mudassarkhan@gmail.com>
2026-03-24 23:25:47 +0530

    Initial commit of new project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <Navbar/>
+  <Hero title="Become an Angular Dev" subtitle="Find the Angular job that fits your skills and needs"/>
+  <HomeCards/>
+</template>
+
+<script setup>
+  import Navbar from './components/Navbar.vue';
+  import Hero from './components/Hero.vue';
+  import HomeCards from './components/HomeCards.vue';
+</script>
